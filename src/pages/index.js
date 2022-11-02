899210b6ffc09f3bf308f7e03a808ef3e36fe30e
BrandonHVX <brandongines@gmail.com>
2022-11-02 16:00:38 -0400

    next

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,42 +5,40 @@ import Logo from "../images/Bootcamp.png"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-function encode(data) {
+const encode = (data) => {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
 }
 
 export default class Index extends Component {
   constructor(props) {
     super(props);
-    this.state = { isValidated: false };
+    this.state = { name: "", email: "", message: "" };
   }
 
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
 
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
+
+  handleSubmit = e => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state,
-      }),
+      body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch((error) => alert(error));
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
   };
 
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
 
   render() {
+          const { name, email, message } = this.state;
+
     return (
       <Layout>
     <Seo title="Page two" />
@@ -92,72 +90,26 @@ export default class Index extends Component {
                 <div class="card-header bg-primary text-white"><i class="fa fa-user"></i> Personal Information
                 </div>
                 <div class="card-body">
-                <form
-                name="contact"
-                method="post"
-                action="/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"name"}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"text"}
-                      name={"name"}
-                      onChange={this.handleChange}
-                      id={"name"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"email"}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"email"}
-                      name={"email"}
-                      onChange={this.handleChange}
-                      id={"email"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"message"}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={"message"}
-                      onChange={this.handleChange}
-                      id={"message"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+                <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" value={message} onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
                 </div>
             </div>
         </div>
